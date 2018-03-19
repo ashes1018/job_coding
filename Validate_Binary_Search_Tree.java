c41a899bd69af816569385b0ca8253ab10efdaff
1132274748@qq.com <1132274748@qq.com>
2018-03-19 08:59:22 +0800

    2018.3.19

---
+++ Validate_Binary_Search_Tree.java
@@ -0,0 +1,50 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Validate_Binary_Search_Tree {
+
+	static class TreeNode {
+		     int val;
+		     TreeNode left;
+		     TreeNode right;
+		     TreeNode(int x) { val = x; }
+	}
+	
+	
+	public boolean isValidBST(TreeNode root) {
+		List<Integer> list = new ArrayList<>();
+        if(root.left == null && root.right == null) return true;
+        
+        helper(root ,list);
+//        System.out.println(list);
+        for (int i = 1; i < list.size(); i++) {
+			if(list.get(i) <= list.get(i-1)){
+				return false;
+			}
+        }
+        return true;
+    }
+	
+	public void helper(TreeNode root, List<Integer> list) {
+	 
+		if(root == null){
+			return;
+		}
+		helper(root.left, list);
+		list.add(root.val);
+		helper(root.right, list );
+//		return list;
+	}
+	public static void main(String[] args) {
+		TreeNode root=  new TreeNode(3);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(4);
+		root.left.left = new TreeNode(1);
+		root.left.right = null;
+		root.right.left = null;
+		root.right.right = new TreeNode(3);
+		System.out.println(new Validate_Binary_Search_Tree().isValidBST(root));
+	}
+}
--- unique_binary_search_tree.java
+++ unique_binary_search_tree.java
@@ -2,11 +2,11 @@ package com.leetcode;
 
 public class unique_binary_search_tree {
 
+//	不知道这道题是怎么和卡特兰数联系上的，反正我是联系不上。另外，复习一下二分搜索树的概念，一个节点的左子树上的节点必定都是小于该节点的值的；而右节点上的值必定都是
+//	大于该节点的值的。
 	public int numTrees(int n) {
 		int[] res = new int[n+1];
-		res[0] = 1;
-//		res[1] = 1;
-				
+		res[0] = 1;						
 		int temp = 0 ;
 		for(int i = 1; i <= n; i++){
 			for(int j = 0; j < i; j++){
