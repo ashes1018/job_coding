88ee4ca62443a1bab6ff6d8cd50da56e637a7ec1
ashes1018 <1132274748@qq.com>
2018-04-09 08:46:48 +0800

    2018.4.6

--- Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
+++ Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
@@ -2,14 +2,14 @@ package com.leetcode;
 
 public class Best_Time_to_Buy_and_Sell_Stock_with_Cooldown {
 
+//采用动规的思想。维护sell和buy两个数组。sell[i]表示在i时刻为止，持股状态下，所能获取的最大收益；buy[i]表示到i时刻为止，未持股状态下的最大收益。
 	public int maxProfit(int[] prices) {
 		int n = prices.length;
 		if(prices == null || prices.length ==0 || prices.length == 1){
 			return 0;
 		}
 		
-		
-		
+	
 		int[] buy = new int[n+1];
 		int[] sell = new int[n+1];
 		
@@ -21,13 +21,11 @@ public class Best_Time_to_Buy_and_Sell_Stock_with_Cooldown {
 			buy[i] = Math.max(buy[i-1], sell[i-2]-prices[i]);
 			sell[i] = Math.max(sell[i-1], buy[i-1]+prices[i]);
 		} 
-		return sell[n-1];
-		
-			
+		return sell[n-1];		
     }
 	
 	public static void main(String[] args) {
-		 int[] prices = {1,2,4};
+		 int[] prices = {1,2,3,0,2};
 		 System.out.println(new Best_Time_to_Buy_and_Sell_Stock_with_Cooldown().maxProfit(prices));
 
 	}
